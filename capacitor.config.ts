e66d5d7ece8a9ad30db9890fd983104fcc7be608
stephcurry5218-art <stephcurry5218@gmail.com>
2026-03-31 17:00:03 -0400

    Update appId and appName in capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.a74e8b0634c94df5a1512482077f4a16',
-  appName: 'glosseti',
+  appId: 'com.glosseti.app',
+  appName: 'Glosseti',
   webDir: 'dist',
   server: {
-    url: 'https://a74e8b06-34c9-4df5-a151-2482077f4a16.lovableproject.com?forceHideBadge=true',
-    cleartext: true
+    androidScheme: 'https'
   }
 };
 
